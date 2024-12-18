0cf19d637b873f88d701d3d537277d72610d8e77
poorijak <poorijak35@gmail.com>
2024-12-19 00:59:03 +0700

    add content about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 gap-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus rerum eaque laborum! Nobis, nostrum molestias asperiores nam laudantium veniam a facere nesciunt aliquid quisquam eligendi voluptatum assumenda, error sapiente cumque non ex accusantium, temporibus modi ea labore alias repudiandae provident dolorem! Nostrum nisi obcaecati excepturi dolores natus nihil minima tenetur voluptates cupiditate, cum aperiam tempora. Necessitatibus consectetur dignissimos voluptates exercitationem rerum? Possimus unde commodi minus exercitationem reiciendis sint maiores. Voluptates vitae suscipit hic dicta obcaecati reprehenderit perspiciatis aspernatur maiores inventore.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque debitis necessitatibus eveniet cumque. Magni, eius a similique, facere voluptates autem assumenda quia, atque exercitationem impedit laborum sapiente sed voluptatem dolores earum voluptatum officiis iusto. Adipisci corporis voluptate obcaecati eos cupiditate earum! Ab, quidem eius laudantium rerum impedit repudiandae quibusdam harum.
+        </p>
+    </div>
+  )
+}
+
+export default page
