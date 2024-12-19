152c9486e4890072fe6e1aa0f41dad8c3ac20b1a
poorijak <poorijak35@gmail.com>
2024-12-19 23:47:57 +0700

    change

--- src/app/components/Navbaritem.jsx
+++ src/app/components/Navbaritem.jsx
@@ -7,11 +7,11 @@ import { useSearchParams } from 'next/navigation'
 function Navbaritem({title , param}) {
 
     const searchParams = useSearchParams(); // ดึงค่าจาก url 
-    const type = searchParams.get('type'); // ดึงค่าจาก genre จาก url
+    const genre = searchParams.get('genre'); // ดึงค่าจาก genre จาก url
 
   return (
     <div>
-      <Link className={`hover:text-amber-600 font-semibold ${type === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 round-lg ' : ''} `} href={`/?type=${param}`}>{title}</Link>
+      <Link className={`hover:text-amber-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 round-lg ' : ''} `} href={`/?genre=${param}`}>{title}</Link>
     </div>
   )
 }
