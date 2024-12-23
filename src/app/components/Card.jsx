8ea82e39cd6325e9f23a1e2686ab6549a7053eb5
Poorijak <Poorijak35@gmail.com>
2024-12-23 15:39:48 +0700

    final test-api

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -7,7 +7,7 @@ function card({ result }) {
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:p-4 m-2'>
       <Link href={`/movie/${result.id}`}>
-        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={500} className='sm:rounded-t-lg group-hover:opacity-20 transition-opacity duration-300' alt={result.original_title}></Image>
+        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={500} className='sm:rounded-t-lg group-hover:opacity-20 transition-opacity duration-300' alt={result.original_title || result.name}></Image>
         <div>
           <p className='line-clamp-3 text-md'>{result.overview}</p>
           <h2 className='text-lg font-bold truncate '>{result.original_title || result.name}</h2>
--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -3,15 +3,14 @@ import React from 'react'
 const API_KEY = process.env.API_KEY
 
 export default async function page({ params }) {
-    const movieId = params.id
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`);
+    const { id } = await params
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`);
     const movie = await res.json()
-    console.log(movie)
     
     return (
         <div className='w-11/12 mx-auto mt-10'>
             <div className='flex flex-col md:flex-row'>
-                <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className='rounded-lg'></Image>
+                <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className='rounded-lg' alt={movie.original_title  || movie.name}></Image>
                 <div className="p-5">
                     <h1 className='font-bold mb-4'>{movie.title}</h1>
                     <p className='mb-3'>{movie.overview}</p>
